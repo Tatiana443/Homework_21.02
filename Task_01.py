a9f5dde0678e5509e2b3eb1edda58f8e2b9d78cf
Tatiana443 <tvb399@yandex.ru>
2023-03-06 15:04:36 +0600

    Created Task_01

---
+++ Task_01.py
@@ -0,0 +1,67 @@
+# Дополнить телефонный справочник возможностью изменения и удаления данных.
+# Пользователь также может ввести имя или фамилию, и Вы должны
+# реализовать функционал для изменения и удаления данных
+def all_contacts():
+    with open('phone_book.txt', 'r', encoding='utf8') as data:
+        for line in data:
+            print(line)
+
+# all_contacts()
+
+
+def find_contact(name):
+    with open('phone_book.txt', 'r', encoding='utf8') as data:
+        for line in data:
+            if name in line:
+                print(line)
+# find_contact('Иван')
+
+
+def add_contact(new_contact):
+    with open('phone_book.txt', 'a', encoding='utf8') as data:
+        data.write('\n')
+        data.write(new_contact)
+
+
+def change_contact(data, change_data) -> str:
+    with open('phone_book.txt', 'r', encoding='utf8') as data:
+        old_data = data.read()
+        change_data = old_data.replace(old_data, change_data)
+    with open('phone_book.txt', 'w', encoding='utf8') as data:
+        data.write(change_data)
+        print('Список изменен')
+
+
+def delete_contact(del_data):
+    with open("phone_book.txt", "r", encoding="utf_8") as data:
+        data = data.readlines()
+    with open("phone_book.txt", "w", encoding="utf_8") as data:
+        for line in data:
+            if del_data not in line.strip("\n"):
+                data.write(line)
+
+
+def main_menu(numb):
+    if numb == 1:
+        all_contacts()
+    if numb == 2:
+        name = input('Введите искомое имя: ')
+        find_contact(name)
+    if numb == 3:
+        data = input('Введите новый контакт: ')
+        add_contact(data)
+    if numb == 4:
+        data = input('Введите контакт для замены: ')
+        change_data = input('Введите новый контакт для замены: ')
+        change_contact(data, change_data)
+    if numb == 5:
+        del_data = input('Введите контакт, который должен быть удален: ')
+        delete_contact(del_data)
+
+
+while True:
+    numb = int(input(
+        "Введите 1 - для печати; 2 - для поиска; 3 - для записи; 4 - для изменения; 5 - для удаления; 6 - для выхода: "))
+    if numb == 6:
+        break
+    main_menu(numb)
